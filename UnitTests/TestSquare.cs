6c414b42d7352428d19a290d1f3ab141be938ece
Tin August Levačić <tilevaci@vsite.hr>
2020-04-29 21:37:50 -0700

    added test

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -17,7 +18,12 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
         {
-            throw new NotImplementedException();
+            var ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+
+            ship.Hit(new Square(1, 4));
+            ship.Hit(new Square(1, 5));
+            var hitStatus = ship.Hit(new Square(1, 6));
+            Assert.AreEqual(HitResult.Sunken, hitStatus);
         }
 
     }
