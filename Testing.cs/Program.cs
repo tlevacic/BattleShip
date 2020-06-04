5f00181b9251e0597bb7aba8758be7f8b1539457
Tin August Levačić <tilevaci@vsite.hr>
2020-06-04 18:07:29 +0200

    Second task is finished

--- Testing.cs/Program.cs
+++ Testing.cs/Program.cs
@@ -27,50 +27,123 @@ namespace Testing.cs
             //Shoot next target
             //if testing is true then GetAvailablePlacements is regular, else it gets 1 as parameter.
             Square s= gunner.NextTarget(testing);
-            return fleet.Hit(s);
-            //No need for changing tactics cuz we will stop after first square is hit
-            //gunner.ProcessHitResult(result);
+            var result = fleet.Hit(s);
+            gunner.ProcessHitResult(result);
+            return result;
         }
 
         static void Main(string[] args)
         {
-            Program p = new Program();
-            int numberOfAttempts = 10;
+            Program p;
+            int numberOfAttempts = 1000;
 
             //FIRST TRY
-            int suma = 0;
-            for(int i=0; i < numberOfAttempts; i++)
+            int sunkenShips = 0;
+
+            int[] sumOfSinkingShips = new int[] {0,0,0,0,0,0,0,0,0 };
+
+            for (int i=0; i < numberOfAttempts; i++)
             {
+                p = new Program();
+                int firstTimeHit = 0;
                 int cnt = 1;
-                while (true)
+                List<int> sunkenShipsCnt = new List<int>();
+                // List will be: FirstShipIsSunked SecondShipIsHit SecondShipIsSunked ThirdShipIsHit .... 
+                while (sunkenShips < 10)
                 {
                     HitResult result = p.ShootNextTarget(false);
-                    if (result == HitResult.Hit)
-                        break;
+                    if (result == HitResult.Hit && firstTimeHit==0) //How many times till first hit of some ship
+                        firstTimeHit = cnt;
+                    if (result == HitResult.Sunken)
+                    {
+                        if(sunkenShips > 1)
+                            sunkenShipsCnt.Add(firstTimeHit);
+                        sunkenShipsCnt.Add(cnt);                     //How many times till ship is sunked
+                        sunkenShips++;
+                        firstTimeHit = 0;
+                    }
                     cnt++;
                 }
-                Console.WriteLine("HITTED IN " + cnt + " attempts");
-                suma += cnt;
+
+                for (int j = 0, k=0; j < sumOfSinkingShips.Count(); j++, k+=2)
+                {
+                    int result = sunkenShipsCnt[k + 1] - sunkenShipsCnt[k];
+                    sumOfSinkingShips[j] = sumOfSinkingShips[j] + result;
+                }
+
+                //Console.WriteLine(String.Join(", ", sunkenShipsCnt));     UNCOMMENT TO SEE DATA IN ARRAY WHEN SHIPS IS HIT; SHIP IS SUNKED
+                sunkenShips = 0;
             }
-            Console.WriteLine("FIRST AVERAGE : " + suma/numberOfAttempts);
+            //After all data is stored in array, need to calculate average hit for every ship
+            for(int i=0; i< sumOfSinkingShips.Count(); i++)
+            {
+                if (i < 9)
+                {
+                    Console.WriteLine(i + 1 + " ship hit : " + (i+2) + " sunked : Average is --->>> " + sumOfSinkingShips[i] / numberOfAttempts);
+                }
+            }
 
 
-            //SECODND TRY
-            suma = 0;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //SECOND TRY- all code is same, just another param is sent to ShootNextTarget
+            //FIRST TRY
+            sunkenShips = 0;
+
+            sumOfSinkingShips = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
             for (int i = 0; i < numberOfAttempts; i++)
             {
+                p = new Program();
+                int firstTimeHit = 0;
                 int cnt = 1;
-                while (true)
+                List<int> sunkenShipsCnt = new List<int>();
+                // List will be: FirstShipIsSunked SecondShipIsHit SecondShipIsSunked ThirdShipIsHit .... 
+                while (sunkenShips < 10)
                 {
                     HitResult result = p.ShootNextTarget(true);
-                    if (result == HitResult.Hit)
-                        break;
+                    if (result == HitResult.Hit && firstTimeHit == 0) //How many times till first hit of some ship
+                        firstTimeHit = cnt;
+                    if (result == HitResult.Sunken)
+                    {
+                        if (sunkenShips > 1)
+                            sunkenShipsCnt.Add(firstTimeHit);
+                        sunkenShipsCnt.Add(cnt);                     //How many times till ship is sunked
+                        sunkenShips++;
+                        firstTimeHit = 0;
+                    }
                     cnt++;
                 }
-                Console.WriteLine("HITTED IN " + cnt + " attempts");
-                suma += cnt;
+
+                for (int j = 0, k = 0; j < sumOfSinkingShips.Count(); j++, k += 2)
+                {
+                    int result = sunkenShipsCnt[k + 1] - sunkenShipsCnt[k];
+                    sumOfSinkingShips[j] = sumOfSinkingShips[j] + result;
+                }
+
+                //Console.WriteLine(String.Join(", ", sunkenShipsCnt));     UNCOMMENT TO SEE DATA IN ARRAY WHEN SHIPS IS HIT; SHIP IS SUNKED
+                sunkenShips = 0;
             }
-            Console.WriteLine("SECOND AVERAGE : " + suma / numberOfAttempts);
+            //After all data is stored in array, need to calculate average hit for every ship
+            for (int i = 0; i < sumOfSinkingShips.Count(); i++)
+            {
+                if (i < 9)
+                {
+                    Console.WriteLine(i + 1 + " ship hit : " + (i + 2) + " sunked : Average is --->>> " + sumOfSinkingShips[i] / numberOfAttempts);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
